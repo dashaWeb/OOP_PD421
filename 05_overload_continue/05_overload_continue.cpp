10c7f00d163894b3a35627952815f48781d085a9
Dasha <dashakonopelko@gmail.com>
2024-10-28 17:13:23 +0200

    overload continie

---
+++ 05_overload_continue/05_overload_continue.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include "Vector.h"
+
+using namespace std;
+int main()
+{
+
+    Vector arr(4,1);
+    /*arr.print();
+    arr[0] = 666;
+    arr[1] = 222;
+    arr.print();
+
+    cout << arr[0] << endl;
+    cout << arr[22] << endl;
+    arr[22] = 2;*/
+
+    for (size_t i = 0; i < arr.getSize(); i++)
+    {
+        arr[i] = rand() % 10 + 1;
+    }
+    arr.print();
+
+    // [1:10:2]
+    Vector splice = arr(2, 4);
+    splice.print();
+    splice.pushBack(33);
+    splice.print();
+    splice.pushBack(66);
+    splice.print();
+    splice.pushBack(77);
+    splice.print();
+
+
+}
+
+// Task
+/* До класу Vector додати наступний функціонал.
+* arr    ==> 2 8 5 1 10 5 9 9 3 5
+* splice ==> 5 1 10 22 33 44 
+1. Використовуючи механізм перевантаження оператора +, обєднати два вектора (arr + splice) ==> new vector(2 8 5 1 10 5 9 9 3 5 5 1 10 22 33 44 )
+ Vector operator + (const Vector& other) const;
+2. Використовуючи механізм перевантаження оператора -, знайти унікальні елементи arr які не належать splice (arr - splice) ==> new vector(2 8 9 9 3)
+Vector operator - (const Vector& other) const;
+3. Використовуючи механізм перевантаження оператора *, результат новий вектор у якого кожне значення збільшили в n - разів 
+Vector operator * (const size_t& n) const;
+4. Перетворення до int повертає суму елементів масиву
+explicit operator int() const;
+5. Перетворення до char* повертає значення елементів массива у вигляді рядка
+explicit operator char*() const;
+methods
+6. void pushBack(const int& value); ==> додати в кінець
+7. void pushFront(const int& value); ==> додати на початок
+8. void insert(const int& value, const size_t& index); ==> додати за вказаною позицією
+9. void popBack(); ==> видалити в кінці
+10. void popFront(); ==> видалити на початку
+11. void remove(const size_t& index); ==> видалити за вказаною позицією
+12. int find(const int& value); -1 - якщо немає, повертаємо індекс першого входження елемента 
+*/
---
+++ 05_overload_continue/Vector.cpp
@@ -0,0 +1,110 @@
+﻿#include "Vector.h"
+
+Vector::Vector()
+	:Vector(1)
+{
+}
+
+Vector::Vector(const size_t& size)
+{
+	this->size = size;
+	this->capacity = size;
+	this->arr = new int[this->size] {}; // створили память для масиву і занулили {}
+}
+
+Vector::Vector(const size_t& size, const int& value)
+	:Vector(size)
+{
+	for (size_t i = 0; i < this->size; i++)
+	{
+		arr[i] = value;
+	}
+}
+
+Vector::Vector(const Vector& other)
+	:Vector(other.size)
+{
+	/*this->size = other.size;
+	this->arr = new int[other.size];*/
+
+	for (size_t i = 0; i < this->size; i++)
+	{
+		this->arr[i] = other.arr[i];
+	}
+}
+// a = b;
+Vector& Vector::operator=(const Vector& other)
+{
+	if (this->arr != nullptr)
+		delete[] this->arr;
+	this->size = other.size;
+	this->capacity = this->size;
+	this->arr = new int[other.size];
+
+	for (size_t i = 0; i < this->size; i++)
+	{
+		this->arr[i] = other.arr[i];
+	}
+	return *this;
+}
+
+Vector::~Vector()
+{
+	if (this->arr != nullptr)
+		delete[] this->arr;
+}
+
+void Vector::print() const
+{
+	cout << "\n Array has " << size << " elements: \n\t";
+	for (size_t i = 0; i < this->size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+void Vector::pushBack(const int& value)
+{
+	if (size >= capacity)
+	{
+		resize();
+	}
+	arr[size++] = value;
+}
+
+int Vector::operator[](size_t index) const
+{
+	if (isValidIndex(index))
+		return arr[index];
+	std::cerr << ">>>>>>>>> Error index (get) : " << index << endl;
+	return INT_MIN;
+}
+
+int& Vector::operator[](size_t index)
+{
+	static int errValue;
+	if (isValidIndex(index))
+		return arr[index];
+	std::cerr << ">>>>>>>>> Error index (set) : " << index << endl;
+	return errValue;
+}
+
+Vector Vector::operator()(size_t startIndex, size_t lastIndex) const
+{
+	if (startIndex >= size)
+		return Vector(0);
+	if (lastIndex >= size)
+		lastIndex = size - 1;
+	if (startIndex > lastIndex)
+		std::swap(startIndex, lastIndex);
+	// 10 20 *30 *40 *50 60  // start = 2; last = 4
+
+	Vector tmp(lastIndex - startIndex + 1);
+	for (size_t i = 0; i < tmp.size; i++)
+	{
+		tmp[i] = this->arr[i + startIndex];
+	}
+
+	return tmp;
+}
